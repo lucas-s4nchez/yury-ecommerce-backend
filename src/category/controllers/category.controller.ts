9ab24bb51f73aa16ed533263e4f61015956abade
Lucas <lucassanchezdl@gmail.com>
2023-06-29 12:42:32 -0300

    paginacion al endpoint de categorias

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -3,6 +3,7 @@ import { CategoryService } from "../services/category.service";
 import { HttpResponse } from "../../shared/response/http.response";
 import { DeleteResult, UpdateResult } from "typeorm";
 import { CategoryDTO } from "../dto/category.dto";
+import { OrderType } from "../../shared/types/shared.types";
 
 export class CategoryController {
   constructor(
@@ -11,11 +12,34 @@ export class CategoryController {
   ) {}
   async getCategories(req: Request, res: Response) {
     try {
-      const data = await this.categoryService.findAllCategories();
-      if (data.length === 0) {
+      let { page = 1, limit = 5, order = OrderType.ASC as any } = req.query;
+      let pageNumber = parseInt(page as string, 10);
+      let limitNumber = parseInt(limit as string, 10);
+
+      // Validar si los valores son numéricos y mayores a 0
+      if (isNaN(pageNumber) || pageNumber <= 0) {
+        pageNumber = 1; // Valor predeterminado si no es un número válido
+      }
+      if (isNaN(limitNumber) || limitNumber <= 0) {
+        limitNumber = 5; // Valor predeterminado si no es un número válido
+      }
+
+      // Validar el valor de order
+      if (!(order in OrderType)) {
+        order = OrderType.ASC; // Valor predeterminado si no es válido
+      }
+
+      const data = await this.categoryService.findAllCategories(
+        pageNumber,
+        limitNumber,
+        order
+      );
+
+      if (data[0].length === 0) {
         return this.httpResponse.NotFound(res, "No hay categorías");
       }
-      return this.httpResponse.Ok(res, data);
+      const [categories, count, totalPages] = data;
+      return this.httpResponse.Ok(res, { categories, count, totalPages });
     } catch (e) {
       return this.httpResponse.Error(res, e);
     }
--- src/category/services/category.service.ts
+++ src/category/services/category.service.ts
@@ -2,18 +2,36 @@ import { DeleteResult, UpdateResult } from "typeorm";
 import { BaseService } from "../../config/base.service";
 import { CategoryDTO } from "../dto/category.dto";
 import { CategoryEntity } from "../entities/category.entity";
+import { OrderType } from "../../shared/types/shared.types";
 
 export class CategoryService extends BaseService<CategoryEntity> {
   constructor() {
     super(CategoryEntity);
   }
 
-  async findAllCategories(): Promise<CategoryEntity[]> {
-    return (await this.execRepository).find();
+  async findAllCategories(
+    page: number,
+    limit: number,
+    order: OrderType
+  ): Promise<[CategoryEntity[], number, number]> {
+    const skipCount = (page - 1) * limit;
+    const [categories, count] = await (await this.execRepository)
+      .createQueryBuilder("categories")
+      .orderBy("categories.name", order)
+      .skip(skipCount)
+      .take(limit)
+      .getManyAndCount();
+
+    const totalPages = Math.ceil(count / limit);
+
+    return [categories, count, totalPages];
   }
 
   async findCategoryById(id: string): Promise<CategoryEntity | null> {
-    return (await this.execRepository).findOneBy({ id });
+    return (await this.execRepository)
+      .createQueryBuilder("category")
+      .where({ id })
+      .getOne();
   }
 
   async findCategoryByName(name: string): Promise<CategoryEntity | null> {
